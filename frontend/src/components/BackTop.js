1a77b8ac5799527aad186af544fcac0720e5bfd3
vanhung88 <88levanhung@gmail.com>
2022-03-22 23:12:36 +0700

    format code

--- frontend/src/components/BackTop.js
+++ frontend/src/components/BackTop.js
@@ -1,37 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './BackTop.css';
 
-const BackTop = () =>{
+const BackTop = () => {
+  const [back, setBack] = useState(false);
 
-    const [back, setBack] = useState(false);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, []);
-
-    const changeBack = () =>{
-        if(window.scrollY>=100){
-            setBack(true);
-        }
-        else{
-            setBack(false);
-        }
-    };
-
-    const onClickBtn = () =>{
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+  const changeBack = () => {
+    if (window.scrollY >= 100) {
+      setBack(true);
+    } else {
+      setBack(false);
     }
+  };
 
-    window.addEventListener('scroll', changeBack);
+  const onClickBtn = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
-    return (
-        <div className="back-top">
-            <button onClick={onClickBtn} id={back ? 'back-tran' : 'back-btn'} className="ui circular icon button">
-                <i style={{width: '20px', height: '20px'}} className="fas fa-chevron-up" />
-            </button>
-        </div>
-        
-    );
+  window.addEventListener('scroll', changeBack);
+
+  return (
+    <div className="back-top">
+      <button
+        onClick={onClickBtn}
+        id={back ? 'back-tran' : 'back-btn'}
+        className="ui circular icon button"
+      >
+        <i
+          style={{ width: '20px', height: '20px' }}
+          className="fas fa-chevron-up"
+        />
+      </button>
+    </div>
+  );
 };
 
 export default BackTop;
